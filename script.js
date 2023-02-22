8303f9b67e77ea2a7a51c059d8fbeb26dea6d281
Deeksha <deekshakarthik09@gmail.com>
2023-02-22 21:34:36 +0530

    progress on script.js

--- script.js
+++ script.js
@@ -4,11 +4,57 @@ const form = document.getElementById('form')
 const search = document.getElementById('search')
 
 async function getUser(username) {
-    try { }
-    catch (err) {
+    try {
+        const { data } = await axios(APIURL + username)
+        console.log(data)
+        createUserCard(data)
+        getRepos(username)
+    }
+    catch (error) {
         if (error.response.status == 404) {
             createErrorCard("No profile with this username!!")
         }
     }
+}
+async function getRepos(username) {
+    try {
+        const { repo } = await axios(APIURL + username + "/repos?sort=created")
+        addReposToCard(repo)
+    }
+    catch (err) {
+        createErrorCard("Problem Fetching Repos! Please try again later!")
+    }
+}
 
+function createErrorCard(msg) {
+    const cardhtml = `
+    <div class="card">
+        <h1>${msg}</h1>
+    </div>
+`
+    main.innerHTML = cardhtml
+}
+
+function createUserCard(user) {
+    const userid =user.name||user.login
+    const userbio = user.bio ? `<p>${user.bio}</p>`:""
+    const cardhtml=
+    `
+        <div class="card">
+        <div>
+          <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+        </div>
+        <div class="user-info">
+          <h2>${userid}</h2>
+          ${userbio}
+          <ul>
+            <li>${user.followers} <strong>Followers</strong></li>
+            <li>${user.following} <strong>Following</strong></li>
+            <li>${user.public_repos} <strong>Repos</strong></li>
+          </ul>
+          <div id="repos"></div>
+        </div>
+      </div>
+        ` 
+main.innerHTML=cardhtml
 }
